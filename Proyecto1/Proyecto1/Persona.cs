82a10619e59d4b2b57170502d4591de8c3902e62
jose villalba <josvilsan3@alu.edu.gva.es>
2026-09-21 19:52:40 +0200

    Creacion Persona

---
+++ Proyecto1/Proyecto1/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto1
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+        public string MostrarDatos()
+        {
+            return $"Nombre: {Nombre}, Edad: {Edad}";
+        }
+    }
+}
